0a1cdebb21ebff5ccd7d771cf542199338ea5a58
Wendy Nieuwkamer <wendy.nieuwkamer@student.auc.nl>
2016-04-07 20:28:48 +0200

    Removing posts is now possible

--- blog/views.py
+++ blog/views.py
@@ -3,13 +3,16 @@ from django.utils import timezone
 from .models import Post
 from .forms import PostForm
 
+
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
 
 def post_new(request):
     if request.method == "POST":
@@ -22,6 +25,7 @@ def post_new(request):
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
+
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -36,10 +40,20 @@ def post_edit(request, pk):
     else:
             form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html',{'form': form})
+
+
 def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
     return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+
 def post_publish(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.publish()
     return redirect('blog.views.post_detail', pk=pk)
+
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('blog.views.post_list')
