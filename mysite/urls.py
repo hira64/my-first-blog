c80b0beb2fd19ef401bf66bcee6c2e3f5ca15b22
Wendy Nieuwkamer <wendy.nieuwkamer@student.auc.nl>
2016-04-07 20:56:43 +0200

    Users have to login to edit and publish.

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
     url(r'',include('blog.urls')),
-]
+    ]
